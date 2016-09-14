48a11a8885bce41e0054c0bf02f99bf29f432fb4
Unknown <nicolas.vess@gmail.com>
2016-09-14 17:55:36 +0200

    Added IIM logo in the footer + link

--- view/foot.php
+++ view/foot.php
@@ -4,6 +4,21 @@
 
          <p>Développé par Louis Resche et Nicolas Vessiere !</p>
 
+            <div class="imgfoot">
+
+                <a href="http://www.iim.fr"> <img src="images/Logo-IIM.jpg"  alt="IIM"/> </a>
+
+            </div>
+
+
     </div>
+
+
+
+
+
+
+
+
 
 </div>
