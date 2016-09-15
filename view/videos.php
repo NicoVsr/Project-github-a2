d25ca2505819a9ea7780305963e1cf971c00cd5b
louisresche <louis.resche@gmail.com>
2016-09-15 19:49:04 +0200

    New "Videos" page

---
+++ view/videos.php
@@ -0,0 +1,21 @@
+<body>
+<?php include '_topbar.php'; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div id="musicfeed">
+                <h1><i class="fa fa-video-camera"></i> Videos</h1>
+                <div class="block animated fadeInDown">
+                    <div class="row">
+                        <div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
+                            <b class="title"></b>
+
+                           
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
