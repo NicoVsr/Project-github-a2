988a5a10360822c2869f4834f89936c0af7ea1d9
louisresche <louis.resche@gmail.com>
2016-09-15 20:56:20 +0200

    New "Soundcloud Go" Page

---
+++ view/go.php
@@ -0,0 +1,20 @@
+<body>
+<?php include '_topbar.php'; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div id="musicfeed">
+                <h1><i class="fa fa-video-camera"></i> Soundcloud Go</h1>
+                <div class="block animated fadeInDown">
+                    <div class="row">
+
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="espace"></div>
+        </div>
+    </div>
+</div>
