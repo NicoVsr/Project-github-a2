53c517395716870bcda71dc5a0ca8e7e25b69af0
louisresche <louis.resche@gmail.com>
2016-09-15 19:18:50 +0200

    adding text on the about page

--- view/about.php
+++ view/about.php
@@ -4,11 +4,20 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div id="musicfeed">
-                <h1><i class="fa fa-envelope"></i> À Propos</h1>
-                <div class="block animated fadeInDown">
+                <div class="block animated fadeInDown jumbotron">
                     <div class="row">
                         <div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
                             <b class="title"></b>
+
+                            <div>
+                                <h1><i class="fa fa-envelope"></i> À Propos</h1>
+                                <p>SoundCloud est la première plate-forme acoustique sociale du monde, où n'importe qui peut écouter ou créer des sons et les partager depuis n'importe où.</p>
+                                <p>Enregistrez facilement et mettez des sons en ligne sur SoundCloud, puis partagez-les avec vos amis, ou sur vos blogs, sites et tous vos réseaux sociaux.</p>
+                                <p>Vous n'avez pas encore de compte gratuit ?</p>
+                                <p><a class="btn btn-primary btn-lg" href="register.php" role="button">Créez un compte</a></p>
+
+                            </div>
+                            </div>
 
 
 
