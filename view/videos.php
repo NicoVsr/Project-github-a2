fdf56419057f646f500398b5d7c7c1e101aa1453
louisresche <louis.resche@gmail.com>
2016-09-15 20:41:47 +0200

    adding videos on the "videos" page

--- view/videos.php
+++ view/videos.php
@@ -3,19 +3,28 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div id="musicfeed">
-                <h1><i class="fa fa-video-camera"></i> Videos</h1>
-                <div class="block animated fadeInDown">
-                    <div class="row">
-                        <div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
-                            <b class="title"></b>
+                <div id="musicfeed">
+                    <h1><i class="fa fa-video-camera"></i> Videos</h1>
+                    <div class="block animated fadeInDown">
+                        <div class="row">
 
-                           
 
+                                <div class="video-container">
+                                    <iframe width="1280" height="720" src="https://www.youtube.com/embed/jWMDZ3GtJGg?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                                <div class="video-container">
+                                    <iframe width="1280" height="720" src="https://www.youtube.com/embed/-CnzuNamnTE?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                                <div class="video-container">
+                                    <iframe width="1280" height="720" src="https://www.youtube.com/embed/Y5YaTA2LVFE?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                                <div class="video-container">
+                                    <iframe width="1280" height="720" src="https://www.youtube.com/embed/pvoqA1ptq50?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            <div class="espace"></div>
         </div>
     </div>
 </div>
