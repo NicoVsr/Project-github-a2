2ceeaa326804506e3873f98ddbdb5a941871c861
Unknown <nicolas.vess@gmail.com>
2016-09-16 16:47:11 +0200

    Changed button techno

--- view/_genre.php
+++ view/_genre.php
@@ -4,14 +4,14 @@
 
 
 
-       <a  class="btn btn-primary btn-lg" role="button" href="https://soundcloud.com/nicovsr/sets/techno" class="techno"> Techno </a>
-
-
-
-
-
-
        
+
+
+
+       <button type="button" class="btn btn-secondary btn-lg btn-block"> <a href="https://soundcloud.com/nicovsr/sets/techno"> Techno </a> </button>
+
+
+
        <button type="button" class="btn btn-secondary btn-lg btn-block"> <a href="https://soundcloud.com/hugo-dos-santos-3/sets/chillax"> Chill</a> </button>
 
 
