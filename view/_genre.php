bbee1b9ca9e059912408a3356fae51e855da967e
Unknown <nicolas.vess@gmail.com>
2016-09-16 16:45:54 +0200

    Changed button Chill

--- view/_genre.php
+++ view/_genre.php
@@ -2,20 +2,20 @@
 
 <body>
 
-   <div class="premiere">
+
 
        <a  class="btn btn-primary btn-lg" role="button" href="https://soundcloud.com/nicovsr/sets/techno" class="techno"> Techno </a>
 
-   </div>
-
-
-    <div class="deuxieme">
-
-        <a  class="btn btn-primary btn-lg"  role="button" href="https://soundcloud.com/hugo-dos-santos-3/sets/chillax" class="techno"> Chill </a>
 
 
 
-    </div>
+
+
+       
+       <button type="button" class="btn btn-secondary btn-lg btn-block"> <a href="https://soundcloud.com/hugo-dos-santos-3/sets/chillax"> Chill</a> </button>
+
+
+
 
 
    <button type="button" class="btn btn-secondary btn-lg btn-block"> <a href="https://soundcloud.com/user969005911"> Rap </a> </button>
